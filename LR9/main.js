f18ff209571551c874eea86e2b191a8745331735
sheremeta-vladyslav-uzhnu <sheremeta.vladyslav@student.uzhnu.edu.ua>
2024-11-01 13:26:58 +0200

    1

---
+++ LR9/main.js
@@ -0,0 +1,46 @@
+// Змінні та функції
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// Текстові рядки
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+// Обробник подій
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300 * 0.0714286) + " stone"; // Перетворення фунтів у стоуни
+    const temperature = Math.round((94 - 32) * 5 / 9) + " centigrade"; // Перетворення °F у °C
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
